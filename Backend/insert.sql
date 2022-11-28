d560660a71b30214f9f66e3f832fb43fbc40e605
rodrigolrfm <rodrigotharo123@gmail.com>
2022-11-28 15:57:26 -0500

    inserts agregados

--- Backend/insert.sql
+++ Backend/insert.sql
@@ -18,6 +18,7 @@ INSERT INTO Provincia (id, nombreProvincia, idDepartamento) VALUES ('4', 'CORONE
 INSERT INTO Provincia (id, nombreProvincia, idDepartamento) VALUES ('5', 'TOCACHE', '5');
 INSERT INTO Provincia (id, nombreProvincia, idDepartamento) VALUES ('6', 'LIMA', '6');
 INSERT INTO Provincia (id, nombreProvincia, idDepartamento) VALUES ('7', 'UTCUBAMBA', '7');
+INSERT INTO Provincia (id, nombreProvincia, idDepartamento) VALUES ('8', 'BARRANCA', '6');
 
 INSERT INTO Distrito (id, nombreDistrito, idProvincia) VALUES ('1', 'JAEN', '1');
 INSERT INTO Distrito (id, nombreDistrito, idProvincia) VALUES ('2', 'TRUJILLO', '2');
@@ -28,6 +29,9 @@ INSERT INTO Distrito (id, nombreDistrito, idProvincia) VALUES ('6', 'LIMA', '6')
 INSERT INTO Distrito (id, nombreDistrito, idProvincia) VALUES ('7', 'HUACHIPA', '6');
 INSERT INTO Distrito (id, nombreDistrito, idProvincia) VALUES ('8', 'EL TAMBO', '3');
 INSERT INTO Distrito (id, nombreDistrito, idProvincia) VALUES ('9', 'BAGUA GRANDE', '7');
+INSERT INTO Distrito (id, nombreDistrito, idProvincia) VALUES ('10', 'LURIN', '6');
+INSERT INTO Distrito (id, nombreDistrito, idProvincia) VALUES ('11', 'SUPE', '8');
+INSERT INTO Distrito (id, nombreDistrito, idProvincia) VALUES ('12', 'HUANCHACO', '2');
 
 INSERT INTO Proveedor (id, razonSocial, idDistrito) VALUES ('1', 'MOVIL BUS S.A.C', '1');
 INSERT INTO Proveedor (id, razonSocial, idDistrito) VALUES ('2', 'ANDES EXPRESS S.A.C.', '2');
